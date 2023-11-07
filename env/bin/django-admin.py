972fb7bdf8fcc83aae2a990ed28cb01f74226384
Kotesh <Koti321naik@gmail.com>
2023-11-07 08:02:09 +0530

    My 1st commite

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/br922687ttl/Desktop/LearningDjango/carzone-project/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
